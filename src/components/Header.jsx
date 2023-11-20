f9484b51706947164bcc3e8f4a0cbcfb61294bac
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-11-21 00:43:55 +0700

    style: changes header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header({ children }) {
       <div id="main-header-loading"></div>
       <header id="main-header">
         <div id="header-title">
-          <h1>React Events</h1>
+          <h1>React Event Management</h1>
         </div>
         <nav>{children}</nav>
       </header>
